96f1026336327137abace85ba70b42968ebfc845
CarlosJaramilloDesarrollo <docentecesde2019@gmail.com>
2019-10-03 21:10:47 -0500

    Se añadio el puerto

--- server-express-html.js
+++ server-express-html.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static( __dirname + "/public_html")); 
-app.listen(3000);
+const port = process.env.port || 3000
 
-console.log("Server en el puerto 3000");
+app.use(express.static( __dirname + "/public_html")); 
+app.listen(port);
+
+console.log(`Server en el puerto ${port}`);
