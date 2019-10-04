f295a86ece39452246a659c71e23f8c321a6a6f9
CarlosJaramilloDesarrollo <docentecesde2019@gmail.com>
2019-10-03 21:49:59 -0500

    TEMP add get /

--- server-express-html.js
+++ server-express-html.js
@@ -3,7 +3,10 @@ const app = express();
 
 const port = process.env.port || 3000;
 
-app.use(express.static( __dirname + "/public_html")); 
-app.listen(port);
-
-console.log(`Server en el puerto ${port}`);
+app.use(express.static( __dirname + "/public_html"));
+app.get('/', function (req, res) {
+    res.send('Soy el index')
+  });
+app.listen(port, () => {
+    console.log(`Server en el puerto ${port}`);
+});
