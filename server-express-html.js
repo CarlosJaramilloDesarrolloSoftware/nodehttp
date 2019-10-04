6d83b6dade969a6b409a055fb9177e2decff17e1
CarlosJaramilloDesarrollo <docentecesde2019@gmail.com>
2019-10-03 21:31:07 -0500

    Eliminamos node_m

--- server-express-html.js
+++ server-express-html.js
@@ -6,4 +6,4 @@ const port = process.env.port || 3000;
 app.use(express.static( __dirname + "/public_html")); 
 app.listen(port);
 
-//console.log(`Server en el puerto ${port}`);
+console.log(`Server en el puerto ${port}`);
